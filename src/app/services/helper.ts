944ae31075307c6c8f1da053566271167dd9f0c9
MilanProgramadorMM <137338471+MilanProgramadorMM@users.noreply.github.com>
2025-10-23 01:20:47 -0500

    ajuste

--- src/app/services/helper.ts
+++ src/app/services/helper.ts
@@ -1,2 +1,8 @@
-let baserUrl = 'http://localhost:8080'
+//LOCAL
+// let baserUrl = 'http://localhost:8080'
+// export default baserUrl;
+
+
+//DEPLOY
+let baserUrl = 'https://sistema-examenes-backend.onrender.com'
 export default baserUrl;
